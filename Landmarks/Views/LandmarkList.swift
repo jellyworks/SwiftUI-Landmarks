2a0cd4a5590dbeee078f657dbab9b977364182de
wizard113 <yeong.rok.lee@gmail.com>
2022-03-22 01:04:54 +0900

    Done Building Lists and Navigation

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by wizard on 2022/03/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+            List(landmarks){ landmark in
+                NavigationLink{
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by wizard on 2022/03/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinates: CLLocationCoordinate2D
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinates: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+    }
+}
